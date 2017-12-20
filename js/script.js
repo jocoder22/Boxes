605cf49a0991faeccf977e5913311451a4a29d16
Okigbo <okigbookey@gmail.com>
2017-12-20 14:32:20 -0500

    refactor: Add jQeury fadeIn and fadeOut
     Changes to be committed:
    	modified:   index.html
    	modified:   js/script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(".greenBox").fadeOut(1000);
+  $(".redBox").fadeOut(6000);
+  $(".blueBox").fadeOut(12000);
+  $(".redBox").fadeIn(18000);
+  $(".blueBox").fadeIn(20000);
+  $(".greenBox").fadeIn(26000);
+});
