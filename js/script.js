ccedbdcb8889d5a54c74e86aeefc0d2e8f74d792
Okigbo <okigbookey@gmail.com>
2017-12-21 11:15:04 -0500

    refactor: Add fadeToggle
     Changes to be committed:
    	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,24 @@ $(function(){
   $(".greenBox").fadeOut(1000);
   $(".redBox").fadeOut(6000);
   $(".blueBox").fadeOut(12000);
-  $(".redBox").fadeIn(18000);
-  $(".blueBox").fadeIn(20000);
-  $(".greenBox").fadeIn(26000);
+
+  $(".redBox").fadeTo(1000, 0.3);
+  $(".blueBox").fadeTo(2000, 0.6);
+  $(".greenBox").fadeTo(4000, 0.8);
+
+  $(".redBox").fadeToggle(18000);
+  $(".blueBox").fadeToggle(20000);
+  $(".greenBox").fadeToggle(26000);
+  $(".redBox").fadeToggle(18000);
+  $(".blueBox").fadeToggle(20000);
+  $(".greenBox").fadeToggle(26000);
+
+  $(".redBox").fadeIn(1000);
+  $(".blueBox").fadeIn(2000);
+  $(".greenBox").fadeIn(2600);
+
+
+
+
+
 });
