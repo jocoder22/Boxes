7f0ca46797720fbdc3009a3095acb307187c9e3a
Okigbo <okigbookey@gmail.com>
2018-01-09 13:06:12 -0500

    bug: Remove click handler
     Changes to be committed:
    	modified:   js/script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,19 +1,4 @@
 
-var slide = document.getElementById("mm");
-var menu = document.querySelector("#btnn");
-var main = document.querySelector(".main");
-var drawer = document.querySelector("#drawer");
-
-menu.addEventListener('click', function(e) {
-  drawer.classList.toggle('open');
-  e.stopPropagation();
-  //slide.style.transform = "translateX(300px)";
-});
-
-main.addEventListener('click', function() {
-  drawer.classList.remove('open');
-  //slide.style.transform = "translateX(0px)";
-});
 
 var model = [
   {name: 'Robert Treat Center', address: '50 Park Pl,', city: 'Newark, NJ 07102', localLocation:[{lat: 40.739037 , lng: -74.168635}]},
@@ -25,30 +10,30 @@ var model = [
   {name: "Wendy's Place", address: '427 Springfield Ave,', city: 'Newark, NJ 07103', localLocation:[{lat: 40.732158 , lng: -74.199454 }]},
   {name: 'Home Depot', address: '399-443 Springfield Ave,', city: 'Newark, NJ 07103', localLocation:[{lat: 40.731358 , lng: -74.198435 }]}];
 
-console.log(model[0].localLocation[0]);
-var map;
-var marker;
-(function initMap() {
-  map = new google.maps.Map(document.getElementById('mm'), {
-    center: model[0].localLocation[0],
-    zoom: 12
-  });
-
-  for (var i = 0; i < model.length; i++) {
-    var maklocation = model[i].localLocation[0];
-    var makaddress = model[i].address;
-    var maktitle = model[i].name;
-    var makcity = model[i].city;
-    var marker = new google.maps.Marker({
-      position: maklocation,
-      map: map,
-      name: maktitle,
-      animation: google.maps.Animation.DROP,
-      cursor: '<h4>' + maktitle + '</h4>' + makaddress + '<br>' + makcity,
-      id: i
-    });
-  };
-})();
+// console.log(model[0].localLocation[0]);
+// var map;
+// var marker;
+// (function initMap() {
+//   map = new google.maps.Map(document.getElementById('mm'), {
+//     center: model[0].localLocation[0],
+//     zoom: 12
+//   });
+//
+//   for (var i = 0; i < model.length; i++) {
+//     var maklocation = model[i].localLocation[0];
+//     var makaddress = model[i].address;
+//     var maktitle = model[i].name;
+//     var makcity = model[i].city;
+//     var marker = new google.maps.Marker({
+//       position: maklocation,
+//       map: map,
+//       name: maktitle,
+//       animation: google.maps.Animation.DROP,
+//       cursor: '<h4>' + maktitle + '</h4>' + makaddress + '<br>' + makcity,
+//       id: i
+//     });
+//   };
+// })();
 
 // menu.addEventListener('click', function(e) {
 //   drawer.classList.toggle('open');
