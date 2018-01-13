0791dba445028cc9c47b4cfe9e16000ae6e2888a
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-01-13 18:37:09 -0500

    feat: Update google marker latlng property
     On branch master
      Your branch is ahead of 'origin/master' by 2 commits.

--- js/script2.js
+++ js/script2.js
@@ -1,8 +1,5 @@
 
-
-
 //make AJAX call to foursquare API
-var AllPlaces = [];
 var result = function(){
   var AllPlaces = [];
   var myAddress = '383 Springfield Ave, Newark NJ 07103';
@@ -14,7 +11,7 @@ var result = function(){
     var resultParsed = data.response.venues;
     for (var i = 0; i < resultParsed.length; i++) {
       var resp = resultParsed[i]; var llresp = resp.location;
-      if (resp.contact.formattedPhone && resp.name.length <= 16 ) {
+      if (resp.contact.formattedPhone && resp.name.length <= 16 && resp.url) {
         AllPlaces.push({
           name: resp.name,
           Address: llresp.formattedAddress,
@@ -24,107 +21,92 @@ var result = function(){
           lng: llresp.lng,
           latlng:[{lat: llresp.lat, lng: llresp.lng}],
           url: resp.url});
+          if(AllPlaces.length == 8){
+            return false;
+          }
       }
     }
   }).fail(function(xhr, errorType, exception) {
-    alert( xhr.status + " " + errorType+"\n\ " + exception);
+    alert( "Failed to connect to Foursquare API : " + xhr.textstatus + " " + errorType + "\n " + exception);
   });
   return AllPlaces;
-  console.log(AllPlaces);
 }();
 
-console.log(result);
 
-var model = [
-  {name: 'Robert Treat Center', address: '50 Park Pl,', city: 'Newark, NJ 07102', localLocation:[{lat: 40.739037 , lng: -74.168635}]},
-  {name: 'Newark Museum', address: '49 Washington St,', city: 'Newark, NJ 07102', localLocation:[{lat: 40.743108 , lng: -74.171716}]},
-  {name: 'University Hospital', address: '150 Bergen St,', city:'Newark, NJ 07103', localLocation:[{lat: 40.740356 , lng: -74.190241 }]},
-  {name: 'Newark ShopRite', address: '206 Springfield Ave', city:'Newark, NJ 07103', localLocation:[{lat: 40.736670 , lng: -74.186159 }]},
-  {name: 'CityPlex 12', address: '360-394 Springfield Ave,', city: 'Newark, NJ 07103', localLocation:[{lat: 40.733553 , lng: -74.196379 }]},
-  {name: "Applebee Grill", address: '383 Springfield Ave,', city: 'Newark, NJ 07103', localLocation:[{lat: 40.732625 , lng: -74.196325 }]},
-  {name: "Wendy's Place", address: '427 Springfield Ave,', city: 'Newark, NJ 07103', localLocation:[{lat: 40.732158 , lng: -74.199454 }]},
-  {name: 'Home Depot', address: '399-443 Springfield Ave,', city: 'Newark, NJ 07103', localLocation:[{lat: 40.731358 , lng: -74.198435 }]}];
 
-console.log(model);
-console.log(model[0].localLocation[0]);
-var mycenter = result;
-// var mycenter2 = mycenter[0].latlng;
 var map;
 var markers = [];
-(function initMap() {
-  map = new google.maps.Map(document.getElementById('mm'), {
-    center: model[3].localLocation[0],
-    zoom: 15
+
+myresults = [];
+setTimeout (function() {
+  result.forEach(function(item){
+    myresults.push(item);
   });
-  var bounds = new google.maps.LatLngBounds();
-  var infowindowList = new google.maps.InfoWindow();
+}, 600);
 
-  for (var i = 0; i < model.length; i++) {
-    var maklocation = model[i].localLocation[0];
-    var makaddress = model[i].address;
-    var maktitle = model[i].name;
-    var makcity = model[i].city;
-    var marker = new google.maps.Marker({
-      position: maklocation,
-      map: map,
-      name: maktitle,
-      animation: google.maps.Animation.DROP,
-      cursor: '<h4>' + maktitle + '</h4>' + makaddress + '<br>' + makcity,
-      id: i
+
+
+setTimeout (function() {
+  (function initMap() {
+    map = new google.maps.Map(document.getElementById('mm'), {
+      center: myresults[3].latlng[0],
+      zoom: 15
     });
-    markers.push(marker);
-    bounds.extend(markers[i].position);
-    marker.addListener('mouseover', function(){
-      showInfoWindow(this, infowindowList);
-    });
+    var bounds = new google.maps.LatLngBounds();
+    var infowindowList = new google.maps.InfoWindow();
 
-  };
-  map.fitBounds(bounds);
-
-  function showInfoWindow(marker, infowindow) {
-  if (infowindow.marker != marker) {
-      infowindow.marker = marker;
-      infowindow.setContent(marker.cursor);
-      infowindow.open(map, marker);
-      marker.addListener('click', function(){
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function () {marker.setAnimation(null);}, 1000);
+    for (var i = 0; i < myresults.length; i++) {
+      var maklocation = myresults[i].latlng[0];
+      var makaddress = myresults[i].Address;
+      var maktitle = myresults[i].name;
+      var makphone = myresults[i].Phone;
+      var makurl = myresults[i].url;
+      var makstats = "checkinsCount : " +  myresults[i].stats.checkinsCount + ", " + " tipCount : " + myresults[i].stats.tipCount + ", " + " usersCount : " + myresults[i].stats.usersCount;
+      var marker = new google.maps.Marker({
+        position: maklocation,
+        map: map,
+        name: maktitle,
+        animation: google.maps.Animation.DROP,
+        cursor: '<h4>' + maktitle + '</h4>' + makaddress + '<br>' + makphone + '<br>' + makurl + '<br>' + makstats,
+        id: i
+      });
+      markers.push(marker);
+      bounds.extend(markers[i].position);
+      marker.addListener('mouseover', function(){
+        showInfoWindow(this, infowindowList);
       });
 
-      marker.addListener('mouseout', function(){
-        infowindow.close(map, marker);
-      });
+    };
+    map.fitBounds(bounds);
 
+    function showInfoWindow(marker, infowindow) {
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent(marker.cursor);
+        infowindow.open(map, marker);
+        marker.addListener('click', function(){
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+          setTimeout(function () {marker.setAnimation(null);}, 1000);
+        });
+        marker.addListener('mouseout', function(){
+          infowindow.close(map, marker);
+        });
+      }
     }
-  }
-
-})();
-
+  })();
+}, 1000);
 
 var ViewModel = function () {
   var self = this;
   self.placeNames = [];
-  self.myPlaces = ko.observableArray(result);
+  self.myPlaces = ko.observableArray([]);
   self.query1 = ko.observable('');
   self.allMarkers = ko.observableArray([]);
-
-
-
-  self.myPlaces2 = ko.observableArray();
-  model.forEach(function(data) {
-    self.myPlaces2.push(data);
-  });
-
-  markers.forEach(function(data) {
-    self.allMarkers.push(data);
-  });
-
-
-
-
- console.log(self.allMarkers()[1]);
- console.log(self.myPlaces());
-
+  setTimeout(function() {
+    result.forEach(function(item){
+      self.myPlaces.push(item);
+    });
+  }, 600);
 
   self.filterPlace = ko.computed(function () {
     var myFilter = self.query1().toLowerCase();
@@ -136,13 +118,6 @@ var ViewModel = function () {
       });
     }
   });
-
-  // self.bounceMarker = function(marker){
-  //   self.allMarkers().id == marker;
-  //   marker.setAnimation(google.maps.Animation.BOUNCE);
-  //   setTimeout(function () {marker.setAnimation(null);}, 1000);
-  // };
-
 };
 
 ko.applyBindings(new ViewModel());
