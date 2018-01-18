e6c3d835fa5dd9911c2b8583868f3e361d61317d
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-01-17 22:23:56 -0500

    feat: Add code to question4
     Changes to be committed:
    	modified:   Answer.py

--- Answer.py
+++ Answer.py
@@ -59,6 +59,20 @@ question4([[0, 1, 0, 0, 0],
           4)
 and the answer would be 3.
 
+
+def question4(T, r, n1, n2):
+    relation = findRelationship(T)
+    ziprelation = findRelationship(zip(*T))
+    if n1 and n2 in relation:
+        return max(set(ziprelation), key=ziprelation.count)
+    else:
+        print 'Node {} and Node {} do not have common ancestor'.format(n1, n2)
+
+
+
+
+
+
 Question 5
 Find the element in a singly linked list that's m elements from the end. For example, if a linked list has 5 elements, the 3rd element from the end is the 3rd element. The function definition should look like question5(ll, m), where ll is the first node of a linked list and m is the "mth number from the end". You should copy/paste the Node class below to use as a representation of a node in the linked list. Return the value of the node at that position.
 
