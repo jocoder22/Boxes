39bc071a33c1df0e2c3c932220e21268bd57b1df
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-01-17 22:27:55 -0500

    fix: Add missing =  in  helper function question4
     Changes to be committed:
    	modified:   Answer.py

--- Answer.py
+++ Answer.py
@@ -69,7 +69,7 @@ def question4(T, r, n1, n2):
         print 'Node {} and Node {} do not have common ancestor'.format(n1, n2)
 
 def findRelationship(ls):
-    relationshipList []
+    relationshipList = []
     for rows in ls:
         for i, j in enumerate(rows):
             if j == 1:
